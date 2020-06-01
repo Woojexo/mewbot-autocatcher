e9b46b6df8ae5aed6b40e39275e49ed15249ebf6
RussianWaffles <developernixion@gmail.com>
2020-06-01 07:53:57 +0100

    Stopped building with private config

--- zip.js
+++ zip.js
@@ -1,7 +1,7 @@
 const AdmZip = require('adm-zip');
 var zip = new AdmZip();
 
-zip.addLocalFile('config.json');
+zip.addLocalFile('config_default.json', '', 'config.json');
 zip.addLocalFile('index.js');
 zip.addLocalFile('package.json');
 zip.addLocalFile('pokemon.json');
